d55caca02347b06b44dc6225c940e5dc731e6bc9
anshu7sah <anshu7sah@gmail.com>
2024-01-09 22:06:51 +0545

    added port for the deployment of the render

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import express,{Request,Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req:Request, res:Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).send("how are you?");
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("listening on port 5000");
 });
